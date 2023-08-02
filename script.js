3aa1e44b74edbbf9f4fc6c6b9f15ca51c0233970
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2023-08-02 22:45:18 +0530

    Calculetor using Js

---
+++ script.js
@@ -0,0 +1,20 @@
+let string = "";
+let Buttons = document.querySelectorAll("button");
+
+Array.from(Buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      document.getElementById("input").value = string;
+    } else if (e.target.innerHTML == "C") {
+      string = "";
+      document.getElementById("input").value = string;
+    } else if (e.target.innerHTML === "A") {
+      string = string.slice(0, -1);
+      document.querySelector("input").value = string;
+    } else {
+      string = string + e.target.innerHTML;
+      document.getElementById("input").value = string;
+    }
+  });
+});
